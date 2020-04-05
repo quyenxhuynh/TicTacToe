ba73fe0e0dc2ca1154addd19d413783f5a99ecc2
Quyen Huynh <qh8ar@virginia.edu>
2020-04-05 18:12:04 -0400

    edited start scene, added transition from start to game

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,29 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
+    @FXML
+    private Button button;
 
-    public Button button;
+    @FXML
+    private void startButtonClicked(ActionEvent event) throws IOException {
+        if (event.getSource() == button) {
+            Stage stage = (Stage)button.getScene().getWindow();
+            Parent root = FXMLLoader.load(getClass().getResource("game.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
 
-    public void startButtonClicked() {
-        button.setText("Starting Game...");
     }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,8 +13,6 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 
-// SCENE SWITCHING RESOURCE: https://stackoverflow.com/questions/37200845/how-to-switch-scenes-in-javafx
-
 public class Main extends Application {
     Label placeholder;
 
@@ -27,21 +25,10 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
         primaryStage.setTitle("Tic-Tac-Toe");
 
-        Scene s = new Scene(root, 500, 300);
-
-        ScreenController screenController = new ScreenController(s);
-        screenController.add("start", FXMLLoader.load(getClass().getResource( "start.fxml" )));
-        screenController.add("game", FXMLLoader.load(getClass().getResource( "game.fxml" )));
-        screenController.activate("start");
-
+        Scene s = new Scene(root, 600, 400);
         primaryStage.setScene(s);
-
-//
-//        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
-
 
     private class ButtonHandler implements EventHandler<ActionEvent> {
 
